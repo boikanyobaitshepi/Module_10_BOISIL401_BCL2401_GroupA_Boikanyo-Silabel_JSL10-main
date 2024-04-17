2186c5b0942d53574ec161ec56e66d8099892c64
boikanyobaitshepi <156679487+boikanyobaitshepi@users.noreply.github.com>
2024-04-17 17:55:14 +0200

    added return before navigateLabyrinth on room 3

--- Module_10_StudentNo_Classcode_Group_Name-Surname_JSL10-main/escapeVanillaJS.js
+++ Module_10_StudentNo_Classcode_Group_Name-Surname_JSL10-main/escapeVanillaJS.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
         fetch('directions.json') 
             .then(response => response.json())
             .then(directions => {
-                navigateLabyrinth(directions)
+               return navigateLabyrinth(directions)
                     .then(message => {
                         // 🪲 Bug: Incorrect method
                         document.getElementById("room3Result").innerHTML = message;
