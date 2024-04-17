b7f7030578718ea694781699fec54a3bbe39f081
boikanyobaitshepi <156679487+boikanyobaitshepi@users.noreply.github.com>
2024-04-17 19:35:09 +0200

    added comments

--- Module_10_StudentNo_Classcode_Group_Name-Surname_JSL10-main/escapeVanillaJS.js
+++ Module_10_StudentNo_Classcode_Group_Name-Surname_JSL10-main/escapeVanillaJS.js
@@ -1,17 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // 🪲 Bug: Incorrect ID used for attaching the event listener
+    // Attach an event listener to the 'solveRoom1' button
     document.getElementById("solveRoom1").addEventListener("click", () => {
+        //fetch data from the "books.json" file
         fetch('books.json') 
             .then(response => response.json())
             .then(books => {
+                //find the most recent book
                 const mostRecentBook = findMostRecentBook(books);
-                // 🪲 Bug: Incorrect element ID
+                // Display the title of the most recent book
                 document.getElementById("room1Result").textContent = `The key to the next room is: ${mostRecentBook.title}`;
             });
     });
-
+   // Attach an event listener to the 'solveRoom2' button
     document.getElementById("solveRoom2").addEventListener("click", () => {
-        const jsConcepts = new Set(['closure', 'scope', 'hoisting' ,'async']);
+        //Create a set of Javascript concepts
+        const jsConcepts = new Set(['closure', 'scope', 'hoisting', 'async']);
         // 🪲 Bug: What's mssing from JS concepts?
         const reactConcepts = new Set(['components', 'jsx', 'hooks', 'async']);
         // 🪲 Bug: Incorrect function call
@@ -24,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
         fetch('directions.json') 
             .then(response => response.json())
             .then(directions => {
-               return navigateLabyrinth(directions);
+                return navigateLabyrinth(directions);
             })
                     .then(message => {
                         // 🪲 Bug: Incorrect method
@@ -35,12 +38,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function findMostRecentBook(books) {
     // 🪲 Bug: Logic error
-    return books.reduce((mostRecent, book) => new Date(book.published) < new Date(mostRecent.published) ? book : mostRecent);
+    return books.reduce((mostRecent, book) => (new Date(book.published) > new Date(mostRecent.published)) ? book : mostRecent);
 }
 
 function findIntersection(setA, setB) {
     // 🪲 Bug: Incorrect logic
-    const intersection = new Set([...setA].filter(item=> setB.has(item)));
+    const intersection = new Set([...setA].filter(item => setB.has(item)));
     return intersection;
 }
 
@@ -52,4 +55,3 @@ async function navigateLabyrinth(directions) {
     }
     return "Congratulations! You've mastered the essentials of Vanilla JavaScript. Welcome to the world of React, where you'll build powerful and dynamic web applications. Let's dive in!";
 }
-
